ae6fad28c69b0027e8ffb5a39b334dc797551b17
SarahSAH02 <142516914+SarahSAH02@users.noreply.github.com>
2025-03-09 16:48:33 +0100

    Sarah task A

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -2,15 +2,31 @@ package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
 	
+	private String topic;
+
 	// message sent from client to create topic on the broker
+	
 
     public CreateTopicMsg(String user, String topic) {
+    	super(MessageType.CREATETOPIC, user);
+    	this.topic = topic;
 
     }
+    
+    public String getTopic() {
+    	return topic;
+    	
+    }
+    
+    public void setTopic(String topic) {
+    	this.topic = topic;
+    }
+    
+    
+   public String toString() {
+	return "CreateTopicMsg [topic=" + topic + "," + super.toString() + "]";
+	   
+   }
+    
 
-	// TODO: 
-	// Implement object variables - a topic is required
-	
-	// Complete the constructor, get/set-methods, and toString method
-    // as described in the project text	
 }
