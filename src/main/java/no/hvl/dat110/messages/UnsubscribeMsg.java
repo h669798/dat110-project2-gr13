3fb44fc6a1c44c4c5f20fa8a427457c2fa2efa92
SarahSAH02 <142516914+SarahSAH02@users.noreply.github.com>
2025-03-09 16:47:44 +0100

    Sarah task A

--- src/main/java/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -2,16 +2,23 @@ package no.hvl.dat110.messages;
 
 public class UnsubscribeMsg extends Message {
 
-	// message sent from client to unsubscribe on a topic 
+	// message sent from client to unsubscribe on a topic
+	private String topic;
 
     public UnsubscribeMsg(String user, String topic) {
 
     }
 
-	// TODO:
-	// Implement object variables - a topic is required
+    public String getTopic() {
+		return topic;
+	}
 
-	// Complete the constructor, get/set-methods, and toString method
-	// as described in the project text
-	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "UnsubscribeMsg [topic=" + topic + "," + super.toString() + "]";
+	}
 }
