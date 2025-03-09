3ed85ef2cc0437ec8b262a067fbfe9c3d14c23b4
SarahSAH02 <142516914+SarahSAH02@users.noreply.github.com>
2025-03-09 16:47:24 +0100

    Sarah task A

--- src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
@@ -3,15 +3,27 @@ package no.hvl.dat110.messages;
 public class SubscribeMsg extends Message {
 
 	// message sent from client to subscribe on a topic 
+	
+	private String topic;
 
     public SubscribeMsg(String user, String topic) {
+    	
+    	super(MessageType.SUBSCRIBE, user);
+    	this.setTopic(topic);
 
     }
 
-	// TODO:
-	// Implement object variables - a topic is required
+	public String getTopic() {
+		return topic;
+	}
 
-	// Complete the constructor, get/set-methods, and toString method
-	// as described in the project text
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "SubscribeMsg [topic=" + topic + "," + super.toString() + "]";
+	}
 		
 }
